7f3fff27d99e1ca5869e303b0192d787f208b514
prithwish122 <prithwishchatterjee1277@gmail.com>
2025-10-06 00:23:32 +0530

    updated transfer section,added scratch card

--- src/app/dashboard/transfer/page.tsx
+++ src/app/dashboard/transfer/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image"
 import { ConnectWalletModal } from "@/app/components/dashboard/connect-wallet-modal"
+import { TransferFlow} from "@/app/components/transfer/transfer-flow"
 
 export default function TransferPage() {
   return (
     <>
       <ConnectWalletModal />
-      <div className="w-full border-none">
+      <div className="w-full">
         <Image
           src="/images/transfer-banner.png"
           alt="Transfer"
@@ -13,8 +14,10 @@ export default function TransferPage() {
           height={200}
           className="w-full h-auto"
           priority
-          
         />
+         <div className="mx-auto mt-6 w-full max-w-3xl px-4 min-h-[560px]">
+          <TransferFlow />
+        </div>
       </div>
     </>
   )
