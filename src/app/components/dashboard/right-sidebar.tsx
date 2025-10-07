da6b569fd55399d1381c8a71c41bda3722cbfa1d
prithwish122 <prithwishchatterjee1277@gmail.com>
2025-10-07 23:08:02 +0530

    daily tsk modal updated

--- src/app/components/dashboard/right-sidebar.tsx
+++ src/app/components/dashboard/right-sidebar.tsx
@@ -3,7 +3,7 @@ import { ProgressModal } from "./progress-modal"
 
 export function RightSidebar() {
   return (
-    <aside className="w-80 overflow-y-auto p-6 space-y-6">
+    <aside className="w-100 overflow-y-auto p-6 space-y-6">
       <UpcomingEvents />
       <ProgressModal />
     </aside>
